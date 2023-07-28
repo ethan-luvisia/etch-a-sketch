32754f6e3330c9351deec9cc68de110a4e79f830
ethan-luvisia <136591734+ethan-luvisia@users.noreply.github.com>
2023-07-28 05:31:47 -0400

    Foundational two part layer established with 16x16 grid constructed

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let rightSide = document.getElementById("right-side");
+
+
+for (i = 1; i <= 256; i++) {
+let childTile = document.createElement("div");
+childTile.setAttribute("id", "tile");
+rightSide.appendChild(childTile);
+};
+
