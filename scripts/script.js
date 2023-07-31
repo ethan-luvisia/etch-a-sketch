affed0ef53615198e245a91f56e8575e6231117d
ethan-luvisia <136591734+ethan-luvisia@users.noreply.github.com>
2023-07-31 11:34:44 -0400

    Functioning range scale incorporated for sketch-pad, soon to add numbers + colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Coloring on pad code
+
 let squares = document.getElementById("squares");
 let isMouseDown = false;
 
@@ -9,16 +11,47 @@ squares.addEventListener("mouseup", (event) => {
 isMouseDown = false;
 })
 
-let gridSize = 256;
+// SlideRange modification code +
 
-for (i = 1; i <= gridSize; i++) {
-let childTile = document.createElement("div");
-childTile.classList.add("tile");
+let gridSize = 10;
+createGrid();
 
-childTile.addEventListener("mouseover", (event) => {
-    if (isMouseDown) {
- childTile.setAttribute("style", "background-color: blue;");
+let myRange = document.getElementById("my-range");
+console.log(myRange);
+
+myRange.addEventListener("input", (event) => {
+gridSize = parseInt(myRange.value);
+console.log(gridSize);
+createGrid();
+});
+
+function createGrid() {
+
+    squares.innerHTML = "";
+
+    let totalTiles = gridSize * gridSize;
+    let tileSize = 500 / gridSize;
+
+    for (i = 1; i <= totalTiles; i++) {
+        let childTile = document.createElement("div");
+        childTile.classList.add("tile");
+        childTile.style.height = `${tileSize}px`;
+        childTile.style.width = `${tileSize}px`;
+
+        childTile.addEventListener("mouseover", (event) => {
+            if (isMouseDown) {
+            childTile.style.backgroundColor = "blue";
+            }
+        })
+        
+        squares.appendChild(childTile);
     }
-})
-squares.appendChild(childTile);
-};
+    
+    squares.style.gridTemplateColumns = `repeat(${gridSize}, ${tileSize}px)`;
+  squares.style.gridTemplateRows = `repeat(${gridSize}, ${tileSize}px)`;
+  squares.style.height = `${gridSize * tileSize}px`;
+  squares.style.width = `${gridSize * tileSize}px`;
+    
+
+}
+
